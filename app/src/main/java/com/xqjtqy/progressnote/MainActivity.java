7587a6bc48f6ba85f95eed4fe65e2b29a73f3045
0Kirby <jty.1234567890@163.com>
2019-02-17 00:13:25 +0800

    activity jumping add cardView

--- app/src/main/java/com/xqjtqy/progressnote/MainActivity.java
+++ app/src/main/java/com/xqjtqy/progressnote/MainActivity.java
@@ -2,12 +2,23 @@ package com.xqjtqy.progressnote;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        cardView = findViewById(R.id.cardView);
+//
+//        cardView.setRadius(8);//设置图片圆角的半径大小
+//
+//        cardView.setCardElevation(8);//设置阴影部分大小
+//
+//        cardView.setContentPadding(5,5,5,5);//设置图片距离阴影大小
     }
 }
--- app/src/main/java/com/xqjtqy/progressnote/OpeningActivity.java
+++ app/src/main/java/com/xqjtqy/progressnote/OpeningActivity.java
@@ -1,7 +1,11 @@
 package com.xqjtqy.progressnote;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -12,6 +16,12 @@ public class OpeningActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Window _window = getWindow();
+        WindowManager.LayoutParams params = _window.getAttributes();
+        params.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|View.SYSTEM_UI_FLAG_IMMERSIVE;
+        _window.setAttributes(params);
+
         setContentView(R.layout.activity_opening);
         final LinearLayout vis = findViewById(R.id.opening1);
         final LinearLayout invis = findViewById(R.id.opening2);
@@ -37,6 +47,21 @@ public class OpeningActivity extends AppCompatActivity {
 
             }
         });
+        Thread myThread=new Thread(){//创建子线程
+            @Override
+            public void run() {
+                try{
+                    sleep(5000);//使程序休眠五秒
+                    Intent it=new Intent(getApplicationContext(),MainActivity.class);//启动MainActivity
+                    startActivity(it);
+                    finish();//关闭当前活动
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();//启动线程
+
     }
 
 
