67822795b08896cc6dc91ecc68f80af05e293f25
0Kirby <jty.1234567890@163.com>
2020-01-31 22:12:25 +0800

    optimize login

--- app/src/main/java/cn/zerokirby/note/BaseActivity.java
+++ app/src/main/java/cn/zerokirby/note/BaseActivity.java
@@ -1,7 +1,6 @@
 package cn.zerokirby.note;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,9 +11,6 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (ThemeUtil.getThemeId(this) == ThemeUtil.WHITE_THEME) {
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }
         ThemeUtil.setClassTheme(this);
     }
 }
