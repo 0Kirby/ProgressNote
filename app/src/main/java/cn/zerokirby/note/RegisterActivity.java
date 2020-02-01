9766980f6e499888a56cb28f0d51d79bdbe05ef7
0Kirby <jty.1234567890@163.com>
2020-02-01 22:56:20 +0800

    use new login&register UI

--- app/src/main/java/cn/zerokirby/note/RegisterActivity.java
+++ app/src/main/java/cn/zerokirby/note/RegisterActivity.java
@@ -1,8 +1,6 @@
 package cn.zerokirby.note;
 
-import android.app.Activity;
 import android.content.ContentValues;
-import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.Handler;
@@ -14,7 +12,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -26,6 +23,7 @@ import org.json.JSONObject;
 import java.util.Objects;
 
 import cn.zerokirby.note.db.UserDatabaseHelper;
+import cn.zerokirby.note.userData.SystemUtil;
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -33,9 +31,9 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 
-public class LoginActivity extends BaseActivity {
+public class RegisterActivity extends BaseActivity {
 
-    static final int LOGIN = 1;//登录
+    static final int REGISTER = 2;//注册
     String userId;
     private String responseData;
     private String username;
@@ -43,42 +41,39 @@ public class LoginActivity extends BaseActivity {
     private long registerTime;
     private long syncTime;
     private Handler handler;//用于进程间异步消息传递
-    static Activity loginActivity;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        loginActivity = this;
 
         final EditText usernameEditText = findViewById(R.id.username);
         final EditText passwordEditText = findViewById(R.id.password);
-        final Button loginButton = findViewById(R.id.login);
-        final TextView register = findViewById(R.id.register_link);
-        final ImageView imageView = findViewById(R.id.close);
+        final Button registerButton = findViewById(R.id.register_btn);
+        final ImageView imageView = findViewById(R.id.back);
         final ProgressBar progressBar = findViewById(R.id.loading);
 
         handler = new Handler(new Handler.Callback() {
 
             @Override
             public boolean handleMessage(@NonNull Message msg) {//用于异步消息处理
-                if (msg.what == LOGIN) {
-                    Toast.makeText(LoginActivity.this, responseData, Toast.LENGTH_SHORT).show();//显示解析到的内容
+                if (msg.what == REGISTER) {
+                    Toast.makeText(RegisterActivity.this, responseData, Toast.LENGTH_SHORT).show();//显示解析到的内容
                     progressBar.setVisibility(View.GONE);
-                    if (responseData.equals("登录成功！")) {
-                        UserDatabaseHelper userDbHelper = new UserDatabaseHelper(LoginActivity.this, "User.db", null, 1);
+                    if (responseData.equals("注册成功！")) {
+                        UserDatabaseHelper userDbHelper = new UserDatabaseHelper(RegisterActivity.this, "User.db", null, 1);
                         SQLiteDatabase db = userDbHelper.getWritableDatabase();
                         ContentValues values = new ContentValues();//将用户ID、用户名、密码存储到本地
                         values.put("userId", userId);
                         values.put("username", username);
                         values.put("password", password);
                         values.put("lastUse", System.currentTimeMillis());
-                        values.put("registerTime", registerTime);
-                        values.put("lastSync", syncTime);
+                        values.put("registerTime", System.currentTimeMillis());//生成注册时间
                         db.update("User", values, "rowid = ?", new String[]{"1"});
+                        LoginActivity.loginActivity.finish();
                         finish();
                     }
                 }
@@ -100,10 +95,10 @@ public class LoginActivity extends BaseActivity {
             @Override
             public void afterTextChanged(Editable s) {//当两个输入框同时不为空时，按钮生效
                 if (!(usernameEditText.getText().toString().equals("") || passwordEditText.getText().toString().equals(""))) {
-                    loginButton.setEnabled(true);
+                    registerButton.setEnabled(true);
                 } else//否则按钮失效
                 {
-                    loginButton.setEnabled(false);
+                    registerButton.setEnabled(false);
                 }
             }
         };
@@ -118,39 +113,34 @@ public class LoginActivity extends BaseActivity {
         usernameEditText.addTextChangedListener(textWatcher);//给两个输入框增加监控
         passwordEditText.addTextChangedListener(textWatcher);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {//登录按钮事件处理
+            public void onClick(View v) {//注册按钮事件处理
                 progressBar.setVisibility(View.VISIBLE);//显示进度条
                 username = usernameEditText.getText().toString();
                 password = passwordEditText.getText().toString();
-                sendRequestWithOkHttpLogin(username, password);//通过OkHttp发送登录请求
-
-            }
-        });
-
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {//注册事件处理
-                Intent it = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(it);
+                sendRequestWithOkHttpRegister(username, password);//通过OkHttp发送注册请求
             }
         });
     }
 
-    private void sendRequestWithOkHttpLogin(final String username, final String password) {
+    private void sendRequestWithOkHttpRegister(final String username, final String password) {
         new Thread(new Runnable() {
             @Override
             public void run() {//在子线程中进行网络操作
                 try {
-                    OkHttpClient client = new OkHttpClient();//利用OkHttp发送HTTP请求调用服务端登录servlet
-                    RequestBody requestBody = new FormBody.Builder().add("username", username).add("password", password).build();
-                    Request request = new Request.Builder().url("https://0kirby.ga:8443/progress_note_server/LoginServlet").post(requestBody).build();
+                    SystemUtil systemUtil = new SystemUtil();
+                    OkHttpClient client = new OkHttpClient();//利用OkHttp发送HTTP请求调用服务端注册servlet
+                    RequestBody requestBody = new FormBody.Builder().add("username", username).add("password", password)
+                            .add("language", systemUtil.getSystemLanguage()).add("version", systemUtil.getSystemVersion())
+                            .add("display", systemUtil.getSystemDisplay()).add("model", systemUtil.getSystemModel())
+                            .add("brand", systemUtil.getDeviceBrand()).build();
+                    Request request = new Request.Builder().url("https://0kirby.ga:8443/progress_note_server/RegisterServlet").post(requestBody).build();
                     Response response = client.newCall(request).execute();
                     responseData = Objects.requireNonNull(response.body()).string();
                     parseJSONWithJSONObject(responseData);//处理JSON
                     Message message = new Message();
-                    message.what = LOGIN;
+                    message.what = REGISTER;
                     handler.sendMessage(message);//通过handler发送消息请求toast
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -163,10 +153,6 @@ public class LoginActivity extends BaseActivity {
         try {
             JSONObject jsonObject = new JSONObject(jsonData);
             responseData = jsonObject.getString("Result");//取出Result字段
-            if (responseData.equals("登录成功！")) {
-                registerTime = jsonObject.getLong("RegisterTime");
-                syncTime = jsonObject.getLong("SyncTime");
-            }
             userId = jsonObject.getString("Id");//取出ID字段
         } catch (JSONException e) {
             e.printStackTrace();
